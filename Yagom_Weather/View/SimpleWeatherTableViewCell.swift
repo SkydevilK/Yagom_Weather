40c1c65b3ca4081e6639d4fa3ef6006ee1907946
SkydevilK <skydevilk@gmail.com>
2022-01-31 06:45:30 +0900

    [Feat] SimpleWeatherView 부분 구현

--- Yagom_Weather/View/SimpleWeatherTableViewCell.swift
+++ Yagom_Weather/View/SimpleWeatherTableViewCell.swift
@@ -8,20 +8,50 @@
 import UIKit
 
 class SimpleWeatherTableViewCell: UITableViewCell {
-
+    
     @IBOutlet weak var cityName: UILabel!
     @IBOutlet weak var currentTemperature: UILabel!
     @IBOutlet weak var currentHumidity: UILabel!
-    @IBOutlet weak var icon: UIImageView!
+    @IBOutlet weak var iconImageView: UIImageView!
     override func awakeFromNib() {
         super.awakeFromNib()
         // Initialization code
     }
-
+    
     override func setSelected(_ selected: Bool, animated: Bool) {
         super.setSelected(selected, animated: animated)
-
+        
         // Configure the view for the selected state
     }
     
+    func configure(viewModel: SimpleWeatherViewModel?) {
+        guard let viewModel = viewModel else {
+            self.cityName.text = "Error"
+            self.currentTemperature.text = "Error"
+            self.currentHumidity.text = "Error"
+            self.iconImageView.image = CacheData.publicCache.placeholderImage
+            return
+        }
+        self.cityName.text = viewModel.cityName
+        self.currentTemperature.text = viewModel.currentTemperature
+        self.currentHumidity.text = viewModel.currentHumidity
+        
+        guard let url = URL(string: viewModel.iconURL ?? "") else {
+            DispatchQueue.main.async {
+                self.iconImageView?.image = UIImage(systemName: "xmark.circle")!
+            }
+            return
+        }
+        CacheData.publicCache.getImage(url: url) { image in
+            guard let image = image else {
+                DispatchQueue.main.async {
+                    self.iconImageView?.image = UIImage(systemName: "xmark.circle")!
+                }
+                return
+            }
+            DispatchQueue.main.async {
+                self.iconImageView?.image = image
+            }
+        }
+    }
 }
--- Yagom_Weather/View/SimpleWeatherViewController.swift
+++ Yagom_Weather/View/SimpleWeatherViewController.swift
@@ -8,27 +8,39 @@
 import UIKit
 
 class SimpleWeatherViewController: UIViewController {
-
+    private let cityNames = ["Gongju", "Gwangju", "Gumi", "Gunsan", "Daegu", "Daejeon", "Mokpo", "Busan", "Seosan", "Seoul", "Sokcho", "Suwon", "Suncheon", "Ulsan", "Iksan", "Jeonju", "Jeju City", "Cheonan", "Cheongju-si", "Chuncheon"]
+    
+    private var simpleWeatherListViewModel = SimpleWeatherListViewModel(weathers: [])
     @IBOutlet weak var simpleWeatherTableView: UITableView!
     override func viewDidLoad() {
         super.viewDidLoad()
+        WeatherAPI.shared.getWeathers(cities: cityNames, completion: { weathers in
+            self.simpleWeatherListViewModel = SimpleWeatherListViewModel(weathers: weathers)
+            DispatchQueue.main.async {
+                self.simpleWeatherTableView.reloadData()
+            }
+        })
         simpleWeatherTableView.delegate = self
         simpleWeatherTableView.dataSource = self
+        simpleWeatherTableView.rowHeight = 128
         let nibName = UINib(nibName: "SimpleWeatherTableViewCell", bundle: nil)
         simpleWeatherTableView.register(nibName, forCellReuseIdentifier: "simpleWeatherCell")
+       
     }
-
-
+    
 }
 
 extension SimpleWeatherViewController: UITableViewDelegate, UITableViewDataSource {
     func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
-        <#code#>
+        return self.simpleWeatherListViewModel.numberOfRowsInSection(section)
     }
     
     func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        <#code#>
+        if let cell = tableView.dequeueReusableCell(withIdentifier: "simpleWeatherCell", for: indexPath) as? SimpleWeatherTableViewCell {
+            let simpleWeatherViewModel = self.simpleWeatherListViewModel.weatherAtIndex(indexPath.row)
+            cell.configure(viewModel: simpleWeatherViewModel)
+            return cell
+        }
+        return UITableViewCell()
     }
-    
-    
 }
